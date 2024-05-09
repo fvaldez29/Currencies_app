b1de65fdb542358162fe5098ebd462f857d65733
fvaldez29 <fv.2900@hotmail.com>
2024-05-09 13:48:44 -0400

    error fixed and loop menu added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <cmath>
+#include <iomanip>
+#include <math.h>
 
 #include "currencyfs.hpp"
 
@@ -7,11 +8,19 @@ using namespace std;
 
 int main(){
     int dinero, currencies;
+
+    
+do
+{
     cout << "Elige la currency para convertir entre las opciones: \n #1 Euro \n #2 Dollar \n #3 Libra Esterlina \n #4 Peso Colombiano \n #5 Peso Mexicanos \n #6 Peso Dominicano \n";
     cin >> currencies;
 
     cout << "Ingrese el monto de dinero a convertir: \n";
     cin >> dinero;
+
+    cin.clear();
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     switch (currencies)
     {
@@ -38,8 +47,11 @@ int main(){
         dominican(currencies, dinero);
         break;
     default:
+        cout << "Opcion invalidad!" << "\n";
         break;
     }
+} while (currencies != 7);
+    
    
 
 }
